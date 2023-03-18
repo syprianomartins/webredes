87b9f962d36c52ef8aee1277625dae09015be3cc
Mattheus Martins <mattheus_cmartins@hotmail.com>
2023-03-18 20:30:40 -0300

    Criando as páginas de outros tópicos e criando as rotas

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,21 @@ const router = express.Router();
 router.get("/", (req, res) => {
     res.render("index");
 });
+
 router.get("/whatisit", (req, res) => {
     res.render("whatisit");
 });
 
+router.get("/howitworks", (req, res) => {
+    res.render("howitworks");
+});
+
+router.get("/benefits", (req, res) => {
+    res.render("benefits");
+});
+
+router.get("/whatis6e", (req, res) => {
+    res.render("whatis6e");
+});
 
 module.exports = router;
