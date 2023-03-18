8f5c875359993d2cf6784e23defa46ef0190e298
Sypriano <syprianom@gmail.com>
2023-03-17 22:04:51 -0300

    alterações tópicos

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,9 @@ const router = express.Router();
 router.get("/", (req, res) => {
     res.render("index");
 });
+router.get("/whatisit", (req, res) => {
+    res.render("whatisit");
+});
 
 
 module.exports = router;
